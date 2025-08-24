9e6cd9792ba6af09f1b0a4d9bb737d513e42d3fe
João Antôno <zabotte34@gmail.com>
2025-08-24 18:47:15 -0400

    Revert "arrumei"
    This reverts commit d7bb0d6cf8e99eb8a7f8029cb5447733d3af351c.

---
+++ front/Screens/GameScreen/iniciar_quiz.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+from backend.jogo import QuizGame
+import sys
+import ast
+
+if __name__ == "__main__":
+    # Obter argumentos da linha de comando
+    if len(sys.argv) >= 4:
+        dificuldade = int(sys.argv[1])
+        classe = int(sys.argv[2])
+        id_turma = int(sys.argv[3]) if sys.argv[3] != "None" else None
+        perguntas_anteriores = ast.literal_eval(sys.argv[4]) if len(sys.argv) > 4 else []
+        
+        root = tk.Tk()
+        root.title('Quiz - Raízes Ocultas')
+        root.geometry('800x600')
+        
+        # Centralizar a janela
+        root.update_idletasks()
+        width = root.winfo_width()
+        height = root.winfo_height()
+        x = (root.winfo_screenwidth() // 2) - (width // 2)
+        y = (root.winfo_screenheight() // 2) - (height // 2)
+        root.geometry(f'{width}x{height}+{x}+{y}')
+        
+        quiz_game = QuizGame(
+            root, 
+            nivel=f'{dificuldade}-{classe}', 
+            id_turma=id_turma,
+            perguntas_anteriores=perguntas_anteriores
+        )
+        
+        root.mainloop()
